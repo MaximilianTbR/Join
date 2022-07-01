5db02261ca85f4814badc8913f7fd724f7baa570
Maximilian Tauber <86464320+MaximilianTbR@users.noreply.github.com>
2022-07-01 15:36:12 +0200

    ninth commit

--- board.js
+++ board.js
@@ -38,3 +38,33 @@ function showMenuButton4() {
 function removeMenuButton4() {
     document.getElementById('menu-point4').classList.add('d-none')
 }
+
+// drag, pull, drop functions for todo's
+
+function dragStart(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+}
+
+function dragEnter(event) {
+    if (event.target.className == "droptarget") {
+        document.getElementById("demo").innerHTML = "Entered the dropzone";
+        event.target.style.border = "3px dotted red";
+    }
+}
+
+function dragLeave(event) {
+    if (event.target.className == "droptarget") {
+        document.getElementById("demo").innerHTML = "Left the dropzone";
+        event.target.style.border = "";
+    }
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("Text/plain");
+    event.target.appendChild(document.getElementById('new-task'));
+}
