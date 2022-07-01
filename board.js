0c76020b01037243460931c973fed764645080df
Maximilian Tauber <86464320+MaximilianTbR@users.noreply.github.com>
2022-07-01 08:34:03 +0200

    eigth commit

--- board.js
+++ board.js
@@ -4,3 +4,37 @@ let toDo = [];
 let inProgress = [];
 let testing = [];
 let done = [];
+
+// Menu functions 
+
+function showMenuButton1() {
+    document.getElementById('menu-point').classList.remove('d-none');
+}
+
+function removeMenuButton1() {
+    document.getElementById('menu-point').classList.add('d-none');
+}
+
+function showMenuButton2() {
+    document.getElementById('menu-point2').classList.remove('d-none');
+}
+
+function removeMenuButton2() {
+    document.getElementById('menu-point2').classList.add('d-none');
+}
+
+function showMenuButton3() {
+    document.getElementById('menu-point3').classList.remove('d-none');
+}
+
+function removeMenuButton3() {
+    document.getElementById('menu-point3').classList.add('d-none');
+}
+
+function showMenuButton4() {
+    document.getElementById('menu-point4').classList.remove('d-none')
+}
+
+function removeMenuButton4() {
+    document.getElementById('menu-point4').classList.add('d-none')
+}
