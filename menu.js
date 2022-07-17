8e771b71b24e4991c6a23eea5a0bb5b53b082ce5
Maximilian Tauber <maximilian.tauber@icloud.com>
2022-07-18 00:13:42 +0100

    made the menu responsive

---
+++ menu.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Functions to open the menu 
+ *  
+ * no parameters
+ * */
+
+
+function openMenu() {
+    document.getElementById('menu').classList.remove('d-none');
+}
+
+function closeMenu() {
+    document.getElementById('menu').classList.add('d-none');
+}
