2a7d988fbd3e0f50fcdae24748d1494491da4a8b
Maximilian Tauber <86464320+MaximilianTbR@users.noreply.github.com>
2022-08-12 23:38:34 +0200

    added functions to add white border

--- menu.js
+++ menu.js
@@ -13,3 +13,29 @@ function openMenu() {
 function closeMenu() {
     document.getElementById('menu').classList.add('d-none');
 }
+
+/**
+ * 
+ * Functions to add white border next to single href in menu 
+ *  
+ * no parameters
+ * */
+
+function menuInit() {
+    if (window.location.href == 'https://gruppe-271.developerakademie.net/Join/board.html') {
+        document.getElementById('board-menu-point').classList.remove('single-href');
+        document.getElementById('board-menu-point').classList.add('single-href-active');
+    }
+    if (window.location.href == 'https://gruppe-271.developerakademie.net/Join/backlog.html') {
+        document.getElementById('backlog-menu-point').classList.remove('single-href');
+        document.getElementById('backlog-menu-point').classList.add('single-href-active');
+    }
+    if (window.location.href == 'https://gruppe-271.developerakademie.net/Join/addTask.html') {
+        document.getElementById('addtask-menu-point').classList.remove('single-href');
+        document.getElementById('addtask-menu-point').classList.add('single-href-active');
+    }
+    if (window.location.href == 'https://gruppe-271.developerakademie.net/Join/help.html') {
+        document.getElementById('help-menu-point').classList.remove('single-href');
+        document.getElementById('help-menu-point').classList.add('single-href-active');
+    }
+}
